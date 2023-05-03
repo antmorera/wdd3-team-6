bb0299ff9ed64165add8e08f5606f90b4a989dee
carteraymond <craymond78@gmail.com>
2023-05-03 14:00:57 -0400

    First attempt at Week2 Team Activity WIP

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,5 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
+import { productDetails } from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const productId = e.target.dataset.id;
-  const product = await findProductById(productId);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParams("product");
+productDetails(productId);
