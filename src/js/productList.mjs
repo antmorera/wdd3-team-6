06d9659949436c0de33caaa745210ef70a1543ff
carteraymond <craymond78@gmail.com>
2023-05-17 13:15:49 -0400

    Week5 assignment

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,38 +1,26 @@
 import { getData } from "./productData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
 
-
-export  default async function productList(selector, category) {
-
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
   const products = await getData(category);
-  if (Array.isArray(products) && products.length > 0) {
-    const htmlItems = products.map((item) => productCardTemplate(item));
-    document.querySelector(selector).innerHTML = htmlItems.join("");
-
-   } //else {
-
-     //   const htmlItems = products.Result.map((item) => productCardTemplate(item));
-     //   document.querySelector(selector).innerHTML = htmlItems.join("");
-
-
-     // }
-
-  
+  console.log(products);
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, el, products);
 }
 
 function productCardTemplate(product) {
-  // const productResult = product.Result
-  const productCard = `<li class="product-card">
-  <a href="product_pages/index.html?product=${product.Id}">
-  <img
-    src="${product.Image}"
-    alt="Image of ${product.NameWithoutBrand}"
-  />
-  <h3 class="card__brand">${product.Name}</h3>
-  <h2 class="card__name">${product.NameWithoutBrand}</h2>
-  <p class="product-card__price">${product.FinalPrice}</p></a>
-</li>`
-
-return productCard;
-}       
-
+  return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img
+      src="${product.Image}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+  </li>`;
+}
 
